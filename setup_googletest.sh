347afa1c1583cbb74a621d4dd9605765215c5653
nagatax <4foview@gmail.com>
2019-12-07 13:08:07 +0900

    googletestのバージョンアップ(1.10.0を使用)
    使用するgoogletestのバージョンを1.10.0にする。

--- setup_googletest.sh
+++ setup_googletest.sh
@@ -4,8 +4,8 @@
 set -eu
 
 # Variables
-PACKAGE=release-1.8.1
-PACKAGE_FILE=release-1.8.1.tar.gz
+PACKAGE=release-1.10.0
+PACKAGE_FILE=${PACKAGE}.tar.gz
 TEMP_FOLDER=googletest-${PACKAGE}
 
 # Build Google Test
@@ -18,14 +18,14 @@ cd ..
 
 # Copy Google Test library files
 mkdir -p vendor/googletest/lib
-cp -a ${TEMP_FOLDER}/googlemock/gtest/libgtest.a vendor/googletest/lib/libgtest.a
-cp -a ${TEMP_FOLDER}/googlemock/gtest/libgtest_main.a vendor/googletest/lib/libgtest_main.a
+cp -a ${TEMP_FOLDER}/lib/libgtest.a vendor/googletest/lib/libgtest.a
+cp -a ${TEMP_FOLDER}/lib/libgtest_main.a vendor/googletest/lib/libgtest_main.a
 cp -a ${TEMP_FOLDER}/googletest/include vendor/googletest/include
 
 # Copy Google Mock library files
 mkdir -p vendor/googlemock/lib
-cp -a ${TEMP_FOLDER}/googlemock/libgmock.a vendor/googlemock/lib/libgmock.a
-cp -a ${TEMP_FOLDER}/googlemock/libgmock_main.a vendor/googlemock/lib/¥libgmock_main.a
+cp -a ${TEMP_FOLDER}/lib/libgmock.a vendor/googlemock/lib/libgmock.a
+cp -a ${TEMP_FOLDER}/lib/libgmock_main.a vendor/googlemock/lib/libgmock_main.a
 cp -a ${TEMP_FOLDER}/googlemock/include vendor/googlemock/include
 
 # Clean
